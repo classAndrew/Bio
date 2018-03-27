18909296cb55b80fbae53d756b33c7831235dd1d
classAndrew <andrxwlin@gmail.com>
2018-03-26 23:01:41 -0700

    3 - Organism Pool and Initial Population

--- GeneticAlgorithm/src/universe/Universe.java
+++ GeneticAlgorithm/src/universe/Universe.java
@@ -1,6 +1,10 @@
 package universe;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import universe.entities.Organism;
+import universe.entities.exceptions.NotRandomException;
 
 public class Universe {
 	
@@ -13,13 +17,41 @@ public class Universe {
 	public static final int GENERATION_LIMIT = 4;
 	
 	
-	public static void main (String[] args){
-
-		Organism organism1 = new Organism("helloworld");
-		System.out.println(organism1.dna.genome);
-		Organism organism2 = new Organism("bellosword");
-		System.out.println(organism1.reproduce(organism2).dna.genome.toString() + "\n");
-		System.out.println(new Organism("bellosword").fitness.fitnessScore);
+	public static void main (String[] args) throws NotRandomException{
+		
+	}
+	
+	public static Organism[] InitializePopulation (boolean isRandom, int size) throws NotRandomException {
+		if (isRandom){
+			Organism[] organisms = new Organism[size+1];
+			for (int i = 0; i < size+1; i++){
+				organisms[i] = new Organism(buildRandomGenome());
+			}
+			return organisms;
+		} else{
+			// Still being worked on...
+			throw new NotRandomException("No set of context could be found, use randomness.");
+		}
+	}
+	
+	public static String buildRandomGenome (){
+		int limit = GENOME_LENGTH;
+		Random rnm = new Random();
+		String genomeLet = "";
+		for (int i = 0; i < limit; i++){
+			genomeLet += ALPHABET[rnm.nextInt(ALPHABET.length-1)];
+		}
+		return genomeLet;
+	}
+	public static Organism[] nextGeneration (Organism[] prevGen){
+		int len = prevGen.length;
+		HashMap<Organism, Float> oPool = new HashMap<>();
+		Organism[] newGen = new Organism[len];
+		int[] fitnesses = new int[len];
+		for (int i = 0; i < len-1; i++){
+			// For tommorow
+		}
+		return null;
 	}
 	
 	@Deprecated
---
+++ GeneticAlgorithm/src/universe/entities/exceptions/NotRandomException.java
@@ -0,0 +1,13 @@
+package universe.entities.exceptions;
+
+public class NotRandomException extends Exception{
+
+	public NotRandomException () {
+		super();
+	}
+	
+	public NotRandomException (String sException) {
+		super(sException);
+	}
+	
+}
